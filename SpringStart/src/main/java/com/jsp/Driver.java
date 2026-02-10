246cae6ee22525b5a44f93e099d5c73718ebe9bc
priyanka <prachii8826@gmail.com>
2026-02-10 13:26:25 +0530

    Added First, SpringStart, and Project2 folder

---
+++ SpringStart/src/main/java/com/jsp/Driver.java
@@ -0,0 +1,21 @@
+package com.jsp;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Driver {
+
+    public static void main(String[] args) {
+
+        ConfigurableApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+
+        Person person =  (Person) context.getBean("person");
+
+        person.m1();
+
+        context.close();
+        System.out.println("container closed");
+
+    }
+}
---
+++ SpringStart/src/main/java/com/jsp/Person.java
@@ -0,0 +1,23 @@
+package com.jsp;
+
+public class Person {
+
+//    Life cycle
+    public Person(){
+        System.out.println("Object created");
+    }
+
+    public void m2(){
+        System.out.println("init method");
+    }
+
+    public void m1(){
+        System.out.println("hello spring");
+    }
+
+    public void m3(){
+        System.out.println("destroy method");
+    }
+
+//
+}
